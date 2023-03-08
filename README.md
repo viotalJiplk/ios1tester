b1fb3632cd71fc54e962a66aa08d129d78ccd01b
viotalJiplk <viotal.varecha@outlook.com>
2023-03-08 19:11:52 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tester for "1 project from BUT FIT IOS 2022-23"
+**It is not a production grade product**
+**There are many missing fetures and many bugs.**
+## How to run
+1. Put test.sh file to directory with your mole shell file.
+2. Set execution bit of file test.sh.
+3. Run `./test.sh`
---
+++ test.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+BINL=$(realpath ".")
+ROOT=$(realpath "./TEST")
+
+assert() {
+    if [ "$1" != "$2" ]; then
+        echo "Assertion failed: $1 != $2"
+    else
+        echo "OK"
+    fi
+}
+
+supertouch(){
+    mkdir -p "$(dirname "$1")"
+    touch "$1"
+}
+
+moleadd(){
+    sleep 1
+    if [ "$1" = "-g" ];then
+        supertouch "$3"   
+        $BINL/mole "$1" "$2" "$3"
+    else
+        supertouch "$1"
+        $BINL/mole "$1"
+    fi
+}
+
+export EDITOR=touch
+export MOLE_RC=$ROOT/MOLE_RC
+rm "$MOLE_RC"
+touch "$MOLE_RC"
+echo "Generating files. This may take some time"
+
+sleep 1
+DATE1=$(date '+%Y-%m-%d')
+sleep 1
+moleadd $ROOT/.ssh/config
+moleadd -g bash $ROOT/.bashrc
+moleadd $ROOT/.local/bin/./mole
+moleadd -g bash $ROOT/.bashrc                         # (D)
+moleadd $ROOT/.indent.pro
+moleadd $ROOT/.viminfo
+
+sleep 1
+DATE2=$(date '+%Y-%m-%d')
+sleep 1
+moleadd -g bash $ROOT/.bash_history
+moleadd -g git $ROOT/.gitconfig
+moleadd -g bash $ROOT/.bash_profile                   # (C)
+moleadd -g git $ROOT/proj1/.git/info/exclude
+moleadd $ROOT/.ssh/known_hosts                        # (A)
+moleadd -g git $ROOT/proj1/.git/config
+moleadd -g git $ROOT/proj1/.git/COMMIT_EDITMSG
+moleadd $ROOT/proj1/.git/COMMIT_EDITMSG
+moleadd -g git $ROOT/proj1/.git/config                # (F)
+moleadd -g project $ROOT/proj1/main.c
+moleadd -g project $ROOT/proj1/struct.c
+moleadd -g project $ROOT/proj1/struct.h
+moleadd -g project_readme $ROOT/proj1/README.md
+
+sleep 1
+DATE3=$(date '+%Y-%m-%d')
+sleep 1
+moleadd -g git2 $ROOT/.gitconfig
+moleadd $ROOT/proj1/main.c
+moleadd $ROOT/.bashrc                                 # (E)
+moleadd $ROOT/.indent.pro
+moleadd $ROOT/.vimrc                                  # (B)
+
+echo "Files generated."
+     
+export EDITOR=echo
+cd $ROOT/.ssh || exit
+assert "$($BINL/mole)" "$ROOT/.ssh/known_hosts" 
+assert "$($BINL/mole "$ROOT")" "$ROOT/.vimrc"
+assert "$($BINL/mole -g bash "$ROOT")" "$ROOT/.bash_profile"
+# assert "$($BINL/mole -g bash -b "$DATE2" "$ROOT")" "$ROOT/.bashrc" # NOT YET WORKING
+
+cd $ROOT || exit
+assert "$($BINL/mole -m)" "$ROOT/.bashrc"
+assert "$($BINL/mole -m -g git $ROOT/proj1/.git)" "$ROOT/proj1/.git/config"
+
+export EDITOR=touch
+$BINL/mole -m -g tst
+assert $? 1
+
+$BINL/mole -a 2023-02-16 -b 2023-02-20
+assert $? 1
+
+export EDITOR=echo
+assert "$($BINL/mole list $ROOT)" '.bash_history: bash
+.bash_profile: bash
+.bashrc:       bash
+.gitconfig:    git,git2
+.indent.pro:   -
+.viminfo:      -
+.vimrc:        -'
+assert "$($BINL/mole list -g bash $ROOT)" '.bash_history: bash
+.bash_profile: bash
+.bashrc:       bash'
+assert "$($BINL/mole list -g project,project_readme $ROOT/proj1)" 'main.c:    project
+README.md: project_readme
+struct.c:  project
+struct.h:  project'
